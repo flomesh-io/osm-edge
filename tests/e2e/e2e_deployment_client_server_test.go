84686dd0181a542fd55a6e7b2c6c1af8a31af179
cybwan <baili@flomesh.io>
2022-06-14 08:26:51 +0800

    Increase mas test duration

--- tests/e2e/e2e_deployment_client_server_test.go
+++ tests/e2e/e2e_deployment_client_server_test.go
@@ -26,7 +26,7 @@ var _ = OSMDescribe("Test HTTP traffic from N deployment client -> 1 deployment 
 			HTTPHeaderName = "podname"
 		)
 
-		var maxTestDuration = 180 * time.Second
+		var maxTestDuration = 190 * time.Second
 
 		Context("DeploymentsClientServer", func() {
 			var (
