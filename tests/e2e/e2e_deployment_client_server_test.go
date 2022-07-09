b254319e227732c5dd13d2fa09e3216a17e05ed3
Cybwan <baili@flomesh.io>
2022-07-09 14:28:40 +0800

    Fix pjs load balancing algorithm (#56)
    * Rename demo script's name
    
    * Recover e2e's testcases.
    
    * For demo, remove windows/amd64 images and add linux/arm64 images.
    
    * Fix name of SIDECAR_LOG_LEVEL env
    
    * Fix pjs load balancing algorithm.

--- tests/e2e/e2e_deployment_client_server_test.go
+++ tests/e2e/e2e_deployment_client_server_test.go
@@ -11,7 +11,6 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/openservicemesh/osm/pkg/constants"
 	. "github.com/openservicemesh/osm/tests/framework"
 )
 
@@ -50,11 +49,6 @@ var _ = OSMDescribe("Test HTTP traffic from N deployment client -> 1 deployment 
 			It("Tests HTTP traffic from multiple client deployments to a server deployment", func() {
 				// Install OSM
 				Expect(Td.InstallOSM(Td.GetOSMInstallOpts())).To(Succeed())
-
-				sidecarClass, _ := Td.GetSidecarClass(Td.OsmNamespace)
-				if len(sidecarClass) == 0 || sidecarClass == constants.SidecarClassPipy {
-					Skip("Pipy doesn't support WASM extension")
-				}
 
 				// Server NS
 				Expect(Td.CreateNs(destApp, nil)).To(Succeed())
