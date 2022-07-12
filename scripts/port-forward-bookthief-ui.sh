f19235bfa75d5ab655e4a702136a8d71d231902e
cybwan <baili@flomesh.io>
2022-07-12 08:04:35 +0800

    Optimize port forward scripts

--- scripts/port-forward-bookthief-ui.sh
+++ scripts/port-forward-bookthief-ui.sh
@@ -8,6 +8,14 @@
 source .env
 
 BOOKTHIEF_LOCAL_PORT="${BOOKTHIEF_LOCAL_PORT:-8083}"
+
 POD="$(kubectl get pods --selector app=bookthief -n "$BOOKTHIEF_NAMESPACE" --no-headers | grep 'Running' | awk 'NR==1{print $1}')"
+
+if [ -z "$POD" ]; then
+    echo "Not found pod: bookthief"
+    exit 1
+fi
+
+kubectl wait -n "$BOOKTHIEF_NAMESPACE" --for=condition=ready pod --selector app=bookthief --timeout=900s
 
 kubectl port-forward "$POD" -n "$BOOKTHIEF_NAMESPACE" "$BOOKTHIEF_LOCAL_PORT":14001 --address 0.0.0.0
--- scripts/port-forward-grafana.sh
+++ scripts/port-forward-grafana.sh
@@ -4,4 +4,11 @@ source .env
 
 POD="$(kubectl get pods --selector app="osm-grafana" -n "$K8S_NAMESPACE" --no-headers | grep 'Running' | awk 'NR==1{print $1}')"
 
+if [ -z "$POD" ]; then
+    echo "Not found pod: osm-grafana"
+    exit 1
+fi
+
+kubectl wait -n "$K8S_NAMESPACE" --for=condition=ready pod --selector app=osm-grafana --timeout=900s
+
 kubectl port-forward "$POD" -n "$K8S_NAMESPACE" 3000:3000 --address 0.0.0.0
