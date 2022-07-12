2b784e8995b7b2a7584885ce3b19936720279145
cybwan <baili@flomesh.io>
2022-07-12 18:47:05 +0800

    Revert port forward scripts change

--- scripts/port-forward-bookstore.sh
+++ scripts/port-forward-bookstore.sh
@@ -11,13 +11,5 @@ if [ -z "$backend" ]; then
 fi
 
 POD="$(kubectl get pods --selector app="$backend" -n "$BOOKSTORE_NAMESPACE" --no-headers | grep 'Running' | awk 'NR==1{print $1}')"
-
-if [ -z "$POD" ]; then
-    echo "Not found pod: $backend"
-    exit 1
-fi
-
-kubectl wait -n "$BOOKSTORE_NAMESPACE" --for=condition=ready pod --selector app="$backend" --timeout=900s
-
 kubectl port-forward "$POD" -n "$BOOKSTORE_NAMESPACE" 15000:15000 --address 0.0.0.0
 
--- scripts/port-forward-osm-debug.sh
+++ scripts/port-forward-osm-debug.sh
@@ -7,11 +7,4 @@ source .env
 
 OSM_POD=$(kubectl get pods -n "$K8S_NAMESPACE" --no-headers  --selector app=osm-controller | awk 'NR==1{print $1}')
 
-if [ -z "$OSM_POD" ]; then
-    echo "Not found pod: osm-controller"
-    exit 1
-fi
-
-kubectl wait -n "$K8S_NAMESPACE" --for=condition=ready pod --selector app=osm-controller --timeout=900s
-
 kubectl port-forward -n "$K8S_NAMESPACE" "$OSM_POD"  9092:9092 --address 0.0.0.0
