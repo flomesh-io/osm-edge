7b8436b58fa138a695924e06bf24115e66b07a65
Cybwan <baili@flomesh.io>
2022-11-02 23:23:49 +0800

    Implemented Multi Cluster Feature.

--- pkg/catalog/multi_cluster.go
+++ pkg/catalog/multi_cluster.go
@@ -1,8 +1,13 @@
 package catalog
 
 import (
+	"fmt"
+	"strings"
+
 	mapset "github.com/deckarep/golang-set"
 
+	"github.com/openservicemesh/osm/pkg/constants"
+	"github.com/openservicemesh/osm/pkg/identity"
 	"github.com/openservicemesh/osm/pkg/service"
 	"github.com/openservicemesh/osm/pkg/trafficpolicy"
 )
@@ -17,7 +22,7 @@ func (mc *MeshCatalog) GetExportTrafficPolicy(svc service.MeshService) (*traffic
 		return nil, nil
 	}
 
-	exportTrafficPolicy := new(trafficpolicy.ServiceExportTrafficPolicy)
+	var trafficMatches []*trafficpolicy.ServiceExportTrafficMatch
 
 	trafficMatch := &trafficpolicy.ServiceExportTrafficMatch{
 		Name:     service.ExportedServiceTrafficMatchName(svc.Name, svc.Namespace, uint16(exportedRule.PortNumber), svc.Protocol),
@@ -40,7 +45,51 @@ func (mc *MeshCatalog) GetExportTrafficPolicy(svc service.MeshService) (*traffic
 		}
 	}
 
-	exportTrafficPolicy.TrafficMatches = append(exportTrafficPolicy.TrafficMatches, trafficMatch)
+	trafficMatches = append(trafficMatches, trafficMatch)
 
-	return exportTrafficPolicy, nil
+	var trafficRoutingRules []*trafficpolicy.Rule
+	sourcePrincipals := mapset.NewSet()
+
+	// If this acl is corresponding to an HTTP port, wildcard the downstream's identity
+	// because the identity cannot be verified for HTTP traffic. HTTP based acl can
+	// restrict downstreams based on their endpoint's IP address.
+	if strings.EqualFold(svc.Protocol, constants.ProtocolHTTP) {
+		sourcePrincipals.Add(identity.WildcardPrincipal)
+	}
+
+	// Build the routing rule for this backend and source combination.
+	// Currently AccessControl only supports a wildcard HTTP route. The
+	// 'Matches' field in the spec can be used to extend this to perform
+	// stricter enforcement.
+	backendCluster := service.WeightedCluster{
+		ClusterName: service.ClusterName(svc.SidecarLocalClusterName()),
+		Weight:      constants.ClusterWeightAcceptAll,
+	}
+	protocol := strings.ToLower(trafficMatch.Protocol)
+	if protocol == constants.ProtocolHTTP || protocol == constants.ProtocolGRPC {
+		routingRule := &trafficpolicy.Rule{
+			Route: trafficpolicy.RouteWeightedClusters{
+				HTTPRouteMatch:   trafficpolicy.WildCardRouteMatch,
+				WeightedClusters: mapset.NewSet(backendCluster),
+			},
+			AllowedPrincipals: sourcePrincipals,
+		}
+		trafficRoutingRules = append(trafficRoutingRules, routingRule)
+	}
+
+	// Create an inbound traffic policy from the routing rules
+	// TODO(#3779): Implement HTTP route matching from AccessControl.Spec.Matches
+	var httpRoutePolicy *trafficpolicy.InboundTrafficPolicy
+	if trafficRoutingRules != nil {
+		httpRoutePolicy = &trafficpolicy.InboundTrafficPolicy{
+			Name:      fmt.Sprintf("%s_from_%s", svc, "IngressController"),
+			Hostnames: []string{"*"},
+			Rules:     trafficRoutingRules,
+		}
+	}
+
+	return &trafficpolicy.ServiceExportTrafficPolicy{
+		TrafficMatches:    trafficMatches,
+		HTTPRoutePolicies: []*trafficpolicy.InboundTrafficPolicy{httpRoutePolicy},
+	}, nil
 }
