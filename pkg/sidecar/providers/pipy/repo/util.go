0e48095823faa36d444818b09ee3e25e42046bae
Cybwan <baili@flomesh.io>
2022-11-15 12:44:24 +0800

    Supported ingress backend service's port different with target port.

--- pkg/sidecar/providers/pipy/repo/util.go
+++ pkg/sidecar/providers/pipy/repo/util.go
@@ -392,7 +392,10 @@ func generatePipyIngressTrafficRoutePolicy(_ catalog.MeshCataloger, _ identity.S
 	for _, trafficMatch := range ingressPolicy.TrafficMatches {
 		tm := itp.getTrafficMatch(Port(trafficMatch.Port))
 		if tm == nil {
-			continue
+			tm = itp.newTrafficMatch(Port(trafficMatch.Port))
+			protocol := strings.ToLower(trafficMatch.Protocol)
+			tm.setProtocol(Protocol(protocol))
+			tm.setPort(Port(trafficMatch.Port))
 		}
 
 		var securitySpec *SourceSecuritySpec
@@ -534,7 +537,10 @@ func generatePipyAccessControlTrafficRoutePolicy(_ catalog.MeshCataloger, _ iden
 	for _, trafficMatch := range aclPolicy.TrafficMatches {
 		tm := itp.getTrafficMatch(Port(trafficMatch.Port))
 		if tm == nil {
-			continue
+			tm = itp.newTrafficMatch(Port(trafficMatch.Port))
+			protocol := strings.ToLower(trafficMatch.Protocol)
+			tm.setProtocol(Protocol(protocol))
+			tm.setPort(Port(trafficMatch.Port))
 		}
 
 		var securitySpec *SourceSecuritySpec
