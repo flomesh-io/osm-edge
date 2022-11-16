0ca1cd9a03e70a70f3ea07be29b99cc76d239539
Cybwan <baili@flomesh.io>
2022-11-16 11:40:24 +0800

    Move pjs to subdirectory: codebase

--- pkg/sidecar/providers/pipy/repo/codebase.go
+++ pkg/sidecar/providers/pipy/repo/codebase.go
@@ -4,89 +4,89 @@ import (
 	_ "embed"
 )
 
-//go:embed codebase_outbound-tcp-load-balance.js
+//go:embed codebase/outbound-tcp-load-balance.js
 var codebaseOutboundTCPLoadBalanceJs []byte
 
-//go:embed codebase_logging-init.js
+//go:embed codebase/logging-init.js
 var codebaseLoggingInitJs []byte
 
-//go:embed codebase_utils.js
+//go:embed codebase/utils.js
 var codebaseUtilsJs []byte
 
-//go:embed codebase_tracing-init.js
+//go:embed codebase/tracing-init.js
 var codebaseTracingInitJs []byte
 
-//go:embed codebase_metrics-http.js
+//go:embed codebase/metrics-http.js
 var codebaseMetricsHTTPJs []byte
 
-//go:embed codebase_config.js
+//go:embed codebase/config.js
 var codebaseConfigJs []byte
 
-//go:embed codebase_tracing.js
+//go:embed codebase/tracing.js
 var codebaseTracingJs []byte
 
-//go:embed codebase_metrics-init.js
+//go:embed codebase/metrics-init.js
 var codebaseMetricsInitJs []byte
 
-//go:embed codebase_logging.js
+//go:embed codebase/logging.js
 var codebaseLoggingJs []byte
 
-//go:embed codebase_metrics-tcp.js
+//go:embed codebase/metrics-tcp.js
 var codebaseMetricsTCPJs []byte
 
-//go:embed codebase_inbound-throttle.js
+//go:embed codebase/inbound-throttle.js
 var codebaseInboundThrottleJs []byte
 
-//go:embed codebase_main.js
+//go:embed codebase/main.js
 var codebaseMainJs []byte
 
-//go:embed codebase_breaker.js
+//go:embed codebase/breaker.js
 var codebaseBreakerJs []byte
 
-//go:embed codebase_inbound-mux-http.js
+//go:embed codebase/inbound-mux-http.js
 var codebaseInboundMuxHTTPJs []byte
 
-//go:embed codebase_outbound-mux-http.js
+//go:embed codebase/outbound-mux-http.js
 var codebaseOutboundMuxHTTPJs []byte
 
-//go:embed codebase_outbound-http-routing.js
+//go:embed codebase/outbound-http-routing.js
 var codebaseOutboundHTTPRoutingJs []byte
 
-//go:embed codebase_inbound-demux-http.js
+//go:embed codebase/inbound-demux-http.js
 var codebaseInboundDemuxHTTPJs []byte
 
-//go:embed codebase_inbound-tls-termination.js
+//go:embed codebase/inbound-tls-termination.js
 var codebaseInboundTLSTerminationJs []byte
 
-//go:embed codebase_outbound-breaker.js
+//go:embed codebase/outbound-breaker.js
 var codebaseOutboundBreakerJs []byte
 
-//go:embed codebase_inbound-proxy-tcp.js
+//go:embed codebase/inbound-proxy-tcp.js
 var codebaseInboundProxyTCPJs []byte
 
-//go:embed codebase_stats.js
+//go:embed codebase/stats.js
 var codebaseStatsJs []byte
 
-//go:embed codebase_outbound-classifier.js
+//go:embed codebase/outbound-classifier.js
 var codebaseOutboundClassifierJs []byte
 
-//go:embed codebase_inbound-http-routing.js
+//go:embed codebase/inbound-http-routing.js
 var codebaseInboundHTTPRoutingJs []byte
 
-//go:embed codebase_outbound-proxy-tcp.js
+//go:embed codebase/outbound-proxy-tcp.js
 var codebaseOutboundProxyTCPJs []byte
 
-//go:embed codebase_codes.js
+//go:embed codebase/codes.js
 var codebaseCodesJs []byte
 
-//go:embed codebase_inbound-classifier.js
+//go:embed codebase/inbound-classifier.js
 var codebaseInboundClassifierJs []byte
 
-//go:embed codebase_inbound-tcp-load-balance.js
+//go:embed codebase/inbound-tcp-load-balance.js
 var codebaseInboundTCPLoadBalanceJs []byte
 
-//go:embed codebase_outbound-demux-http.js
+//go:embed codebase/outbound-demux-http.js
 var codebaseOutboundDemuxHTTPJs []byte
 
-//go:embed codebase_config.json
+//go:embed codebase/config.json
 var codebaseConfigJSON []byte
