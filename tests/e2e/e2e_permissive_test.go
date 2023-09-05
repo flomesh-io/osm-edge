4d4c3e76a8f641a8da44ba9a18f4480a42fc8564
Cybwan <baili@flomesh.io>
2023-09-06 07:58:46 +0800

    fix permissive e2e test.

--- tests/e2e/e2e_permissive_test.go
+++ tests/e2e/e2e_permissive_test.go
@@ -174,7 +174,7 @@ func testPermissiveMode(withSourceKubernetesService bool) {
 		cond = Td.WaitForRepeatedSuccess(func() bool {
 			result := Td.HTTPRequest(req)
 
-			if result.Err == nil || !strings.Contains(result.Err.Error(), "command terminated with exit code 7 ") {
+			if result.Err == nil || !strings.Contains(result.Err.Error(), "command terminated with exit code") {
 				Td.T.Logf("> REST req received unexpected response (status: %d) %v", result.StatusCode, result.Err)
 				return false
 			}
